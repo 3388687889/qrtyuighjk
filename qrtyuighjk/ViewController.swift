425f83f38eb6653df199303dc9680f2c93bd313f
zlh <3388687889>
2022-09-06 17:20:54 +0800

    Initial Commit

---
+++ qrtyuighjk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qrtyuighjk
+//
+//  Created by kys-5 on 2022/9/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
